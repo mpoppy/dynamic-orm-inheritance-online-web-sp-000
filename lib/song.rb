e089282faa6c05165547cc5cdcf88e4034707bc3
Marc Poppy <poppy.marc@gmail.com>
2019-11-17 17:16:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require_relative "./interactive_record.rb"
 
 class Song < InteractiveRecord
 
-  self.column_names.each do |col_name|
+  self.column_names.each do |col_name| #the only code that Song needs is the code to create attr_accessors specific to itself
     attr_accessor col_name.to_sym
   end
 
